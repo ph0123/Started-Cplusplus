bd2b8454d27e69d5852907bd52b652bf12119f27
ph0123 <cnphuongdavid@gmail.com>
2022-11-21 23:20:09 +0100

    update catch2 for unit testing

--- 3. Introducing Functional Programming in C++/Ch01/01_02/main.cpp
+++ 3. Introducing Functional Programming in C++/Ch01/01_02/main.cpp
@@ -2,7 +2,8 @@
 
 // Tells Catch to provide a main()
 #define CATCH_CONFIG_MAIN
-#include "catch.hpp"
+#include <catch2/catch_test_macros.hpp>
+#include <catch2/benchmark/catch_benchmark.hpp>
 
 // a pure function without side effects
 int fibonacci(int n) {
